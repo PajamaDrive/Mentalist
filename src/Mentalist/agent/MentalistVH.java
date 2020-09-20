adbb74cd53f1f9bdd27983cc94864aa5ab207623
PajamaDrive <pajamadrive49@gmail.com>
2020-09-20 21:09:32 +0900

    Mentalist

---
+++ src/Mentalist/agent/MentalistVH.java
@@ -0,0 +1,39 @@
+package Mentalist.agent;
+
+import edu.usc.ict.iago.utils.GameSpec;
+
+import javax.websocket.Session;
+
+
+/**
+ * @author mell
+ * 
+ */
+public class MentalistVH extends MentalistCoreVH {
+
+	/**
+	 * @author mell
+	 * Instantiates a new  VH.
+	 *
+	 * @param name: agent's name
+	 * @param game: gamespec value
+	 * @param session: the session
+	 */
+	public MentalistVH(String name, GameSpec game, Session session)
+	{
+		super("Sympathizer", game, session, new MentalistRepeatedFavorBehavior(MentalistRepeatedFavorBehavior.LedgerBehavior.FAIR), new MentalistRepeatedFavorExpression(),
+				new MentalistRepeatedFavorMessage(false, false, MentalistRepeatedFavorBehavior.LedgerBehavior.FAIR));
+		
+		super.safeForMultiAgent = true;
+	}
+
+	@Override
+	public String getArtName() {
+		return "Rens";
+	}
+
+	@Override
+	public String agentDescription() {
+			return "<h1>Opponent</h1><p>They are excited to begin negotiating!</p>";
+	}
+}
